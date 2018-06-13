133b1c1ac163a3054cba0552ef5722d2baafe636
Uccio <maurizio.panuccio@gmail.com>
2018-06-13 23:19:38 +0200

    ok version

--- mysql_db_backup.bat
+++ mysql_db_backup.bat
@@ -3,6 +3,6 @@ set adesso=%date:~-4%-%date:~3,2%-%date:~0,2%-%time:~0,2%-%time:~3,2%%time:~6,2%
 SET backup_path="c:\Backup_DB"
 SET databaselist = "mydb1 mydb2"
 
-mysqldump -ubackup -plamiapassword --databases %databaselist%  > %BACKUP_PATH%\effegomme6-%ADESSO%.sql
+mysqldump -ubackup -plamiapassword --databases %databaselist%  > %BACKUP_PATH%\backup-%ADESSO%.sql
 ROBOCOPY %BACKUP_PATH% c:\tmp\oldfiles /mov /minage:90
 del C:\tmp\oldfiles /q
